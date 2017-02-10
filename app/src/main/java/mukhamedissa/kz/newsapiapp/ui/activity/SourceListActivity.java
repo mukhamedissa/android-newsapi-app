849f38ee33d3beb716007b5b55ec2776afc5070e
Mukhamed Issa <isa@kolesa.kz>
2017-02-10 15:54:01 +0600

    Added libraries

--- app/src/main/java/mukhamedissa/kz/newsapiapp/ui/activity/SourceListActivity.java
+++ app/src/main/java/mukhamedissa/kz/newsapiapp/ui/activity/SourceListActivity.java
@@ -1,7 +1,9 @@
-package mukhamedissa.kz.newsapiapp;
+package mukhamedissa.kz.newsapiapp.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mukhamedissa.kz.newsapiapp.R;
 
 public class SourceListActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class SourceListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_source_list);
+
+
     }
 }
